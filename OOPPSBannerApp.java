e0e60027fabbf8c2eac33c7d717d67627ee76d77
durga-12-hub <db543952@gmail.com>
2026-04-02 01:35:29 +0530

    Complete OOPS Banner App implementation

--- OOPPSBannerApp.java
+++ OOPPSBannerApp.java
@@ -1,16 +1,60 @@
+/**
+ * OOPSBannerApp UC6 - OOPS Banner Application (Use Case 6)
+ * * This use case extends UC5 by implementing a modular approach to generate each
+ * letter's pattern through dedicated methods. This enhances code reusability and
+ * maintainability by separating pattern generation logic from the main display logic.
+ * * @author Developer
+ * @version 6.0
+ */
+
 public class OOPSBannerApp {
 
+    // Method to generate the pattern for the letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+            "  *** ",
+            " * * ",
+            " * * ",
+            " * * ",
+            "  *** "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+            " ***** ",
+            " * *",
+            " ***** ",
+            " * ",
+            " * "
+        };
+    }
+
+    // Method to generate the pattern for the letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+            " ***** ",
+            " * ",
+            "  *** ",
+            "     * ",
+            " ***** "
+        };
+    }
+
+    // Main method to run the banner display
     public static void main(String[] args) {
 
-        // Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
+        // Declare String Arrays to hold patterns for each letter
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        System.out.println(String.join(" ", " **** ", " ***** "));
-        System.out.println(String.join(" ", "**   **", "**   **"));
-        System.out.println(String.join(" ", "**   **", "**"));
-        System.out.println(String.join(" ", "**   **", " ***** "));
-        System.out.println(String.join(" ", "**   **", "     **"));
-        System.out.println(String.join(" ", "**   **", "**   **"));
-        System.out.println(String.join(" ", " **** ", " ***** "));
-        
+        // Use the loop to Assemble each line of the banner to create the
+        // visual effect for the message "OOPS"
+        // We print: O + O + P + S side-by-side
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+        }
     }
 }
